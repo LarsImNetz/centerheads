33f5ad1ac4a5444e7551759dc51f3704efb42e87
Lars Langhans <lars.langhans@gmx.de>
2017-05-17 22:53:23 +0200

    use good resize instead of make background greater

--- resize.sh
+++ resize.sh
@@ -4,11 +4,12 @@ echo "Search for all png files..."
 PICS=$(find . -maxdepth 1 -iname '*_bigger_90.png')
 
 for PIC in $PICS; do
-    echo "convert to bigger: $PIC"
+    echo "convert resize: $PIC"
     BASENAME=$(basename $PIC)
 
     EXTENSION=${BASENAME#*.}
     PNGNAME="${BASENAME/_bigger_90.${EXTENSION}/.png}"
   
-    convert $PIC -background transparent -gravity center -extent 260x270 $PNGNAME
+#     convert $PIC -background transparent -gravity center -extent 260x270 $PNGNAME
+    convert $PIC -filter spline -resize 260x270 -unsharp 0x1 $PNGNAME
 done
