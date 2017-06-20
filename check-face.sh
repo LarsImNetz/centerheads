ca0041d11e5d032a53e0269800a36adb70b77b1d
Lars Langhans <lars.langhans@hypoport.de>
2017-06-20 11:24:37 +0200

    for each picture create a check-face.txt line
    - normal with facedetect
    - if facedetect fails, create a HANDMADE line where you need to take hands on

--- check-face.sh
+++ check-face.sh
@@ -10,30 +10,30 @@ CHECKFILE=check-face/check-face.txt
 
 touch $CHECKFILE
 
-count=10
+# count=10
 for PIC in $PICS; do
     echo "check for face: $PIC"
 
-    count=$(( $count - 1))
-    if [ $count -le 0 ]; then
-        exit 1
-    fi
+#    count=$(( $count - 1))
+#    if [ $count -le 0 ]; then
+#        exit 1
+#    fi
 
     BASENAME=$(basename $PIC)
     EXTENSION=${BASENAME#*.}
-    PNGNAME="${BASENAME/.${EXTENSION}/_wo.png}"
+    PNGNAME="${BASENAME/.${EXTENSION}/.png}"
 
     ./facedetect -q $PIC
     FACE=$?
 
-    if [ $FACE -eq 0 ]; then
-        read xp yp <<< $(identify -format '%w %h' $PIC)
-        echo "$PIC width: ${xp} height: ${yp}"
+    read xp yp <<< $(identify -format '%w %h' $PIC)
+    echo "$PIC width: ${xp} height: ${yp}"
 
+    if [ $FACE -eq 0 ]; then
         ./facedetect -o check-face/$PNGNAME $PIC
         read x y w h <<< $(./facedetect $PIC)
-#        echo "$PIC GEOM $xp $yp FACE $x $y $w $h" >>$CHECKFILE
-#    else
-#        echo "# $PIC NO-FACE fail with error: $FACE" >>$CHECKFILE
+        echo "$PIC GEOM $xp $yp FACE $x $y $w $h" >>$CHECKFILE
+    else
+        echo "# $PIC GEOM $xp $yp HANDMADE" >>$CHECKFILE
     fi
 done
